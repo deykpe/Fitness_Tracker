9c47d82eb0d34790512c54d952aecd48db92c815
deykpe <dpang2@uci.edu>
2020-01-07 16:36:38 -0800

    starting files

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require ('express');
+var mongoose = require ('mongoose');
+
+var port = 8080
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost/workout', {
+    useNewUrlParser:true, 
+    useFindAndModify:false
+})
+
+app.use(require('./routes'))
+
+
+app.listen(port, function() {
+console.log('port is working on 8080')
+})
+
