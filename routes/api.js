6e97254a80e07ff35734a23151e68b7afd0e003e
deykpe <dpang2@uci.edu>
2020-01-07 16:50:03 -0800

    api

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,37 @@
 const router = require ('express').Router()
 const db = require ('../models/workout1.js')
+
+router.put('/api/workouts/:id', function (request, response){
+    console.log(request.params.id, request.body)
+    db.findByIdAndUpdate(
+         request.params.id,
+     {$push:{exercises:request.body}},{new:true,runValidators:true})
+     .then(function(records){
+        console.log(records)
+        response.json(records)
+    })
+    .catch( function (error){
+        response.send(error)
+    })
+});
+
+router.get('/api/workouts', function (request, response){
+    db.find().then(function(records){
+        response.json(records)
+    })
+    .catch( function (error) {
+        response.send(error)
+    })
+});
+
+router.post('/api/workouts', function (request, response){
+
+    db.create((request.body)).then(function(records){
+        console.log(records)
+        response.json(records)
+    })
+    .catch( function (error){
+        response.send(error)
+    })
+})
+
